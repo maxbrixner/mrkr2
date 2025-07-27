7f67ce51a2af9aad15633f2360110c49fc3a9801
Max Brixner <max.brixner@gmail.com>
2025-07-27 19:29:41 +0200

    common base class for list based content

---
+++ frontend/components/list_based_content.ts
@@ -0,0 +1,176 @@
+/* -------------------------------------------------------------------------- */
+
+import { MessageBox } from './base/message_box.js';
+import { StyledButton } from './base/styled_button.js';
+import { StyledInput } from './base/styled_input.js';
+import { FilteredTable, RowClickedEvent, SelectionChangedEvent, TableRenderErrorEvent } from './base/filtered_table.js';
+
+/* -------------------------------------------------------------------------- */
+
+export interface ListBasedContentAttributes {
+    contentUrl?: string;
+    sortImg?: string;
+}
+
+/* -------------------------------------------------------------------------- */
+
+export class ListBasedContent extends HTMLElement implements ListBasedContentAttributes {
+    protected _searchInput: StyledInput = new StyledInput();
+    protected _table: FilteredTable = new FilteredTable();
+    protected _buttonsDiv: HTMLDivElement = document.createElement('div');
+    protected _filterTimeout: any = null;
+
+    get contentUrl() {
+        return this._table.contentUrl || '';
+    }
+
+    set contentUrl(value: string) {
+        this.setAttribute('content-url', value);
+    }
+
+    get sortImg() {
+        return this._table.sortImg || '';
+    }
+
+    set sortImg(value: string) {
+        this.setAttribute('sort-img', value);
+    }
+
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+
+        this._populateShadowRoot();
+    }
+
+    static get observedAttributes() {
+        return ['content-url', 'project-gui-url', 'scan-url', 'sort-img'];
+    }
+
+    attributeChangedCallback(propertyName: string, oldValue: string | null, newValue: string | null) {
+        if (oldValue === newValue) return;
+
+        if (propertyName === 'content-url') {
+            this._table.contentUrl = newValue || '';
+        } else if (propertyName === 'sort-img') {
+            this._table.sortImg = newValue || '';
+        }
+    }
+
+    connectedCallback() {
+        this.shadowRoot?.addEventListener('row-clicked', (event: Event) => this._onRowClicked(event as CustomEvent<RowClickedEvent>));
+        this.shadowRoot?.addEventListener('selection-changed', (event: Event) => this._onSelectionChanged(event as CustomEvent<SelectionChangedEvent>));
+        this.shadowRoot?.addEventListener('table-render-error', (event: Event) => this._onTableRenderError(event as CustomEvent<TableRenderErrorEvent>));
+        this._searchInput.addEventListener('input', (event: Event) => this._onSearchInputChange(event));
+    }
+
+    disconnectedCallback() {
+        this.shadowRoot?.removeEventListener('row-clicked', (event: Event) => this._onRowClicked(event as CustomEvent<RowClickedEvent>));
+        this.shadowRoot?.removeEventListener('selection-changed', (event: Event) => this._onSelectionChanged(event as CustomEvent<SelectionChangedEvent>));
+        this.shadowRoot?.removeEventListener('table-render-error', (event: Event) => this._onTableRenderError(event as CustomEvent<TableRenderErrorEvent>));
+        this._searchInput.removeEventListener('input', (event: Event) => this._onSearchInputChange(event));
+    }
+
+    protected _getTableConfig() {
+        return '{}';
+    }
+
+    protected _populateShadowRoot() {
+        if (!this.shadowRoot) {
+            throw new Error("Shadow Root is not initialized.");
+        }
+
+        const style = document.createElement('style');
+        style.textContent = `
+            :host {
+                display: grid;
+                grid-template-areas:
+                    "toolbar"
+                    "table";
+                grid-template-rows: auto 1fr;
+            }
+
+            .toolbar {
+                align-items: center;    
+                background-color: var(--background-color);
+                border-bottom: 1px solid var(--border-color);
+                display: grid;
+                grid-area: toolbar;
+                grid-template-areas: "scan search";
+                grid-template-columns: 1fr auto;
+                padding: 0.5rem 1rem;
+            }
+        
+            .toolbar > .buttons {
+                align-items: center;    
+                display: grid;
+                gap: .5rem;
+                grid-auto-columns: min-content;
+                grid-auto-flow: column;
+            }
+        `;
+
+        this.shadowRoot.appendChild(style);
+
+        const toolbar = document.createElement('div');
+        toolbar.classList.add('toolbar');
+        toolbar.style.gridArea = 'toolbar';
+
+        this._buttonsDiv = document.createElement('div');
+        this._buttonsDiv.classList.add('buttons');
+        toolbar.appendChild(this._buttonsDiv);
+
+        this._searchInput.type = "search";
+        this._searchInput.placeholder = "Filter Projects...";
+        this._searchInput.autocomplete = "off";
+        this._searchInput.autocorrect = "off";
+        this._searchInput.autocapitalize = "off";
+        this._searchInput.spellcheck = false;
+        this._searchInput.ariaLabel = "Filter Projects";
+        this._searchInput.style.gridArea = 'search';
+        this._searchInput.addEventListener('input', (event: Event) => this._onSearchInputChange(event));
+        toolbar.appendChild(this._searchInput);
+
+        this._table.style.gridArea = 'table';
+        this._table.ariaLabel = "Projects Table";
+        this._table.config = this._getTableConfig();
+        this._table.emptyMessage = 'No projects found';
+
+        this.shadowRoot.appendChild(toolbar)
+        this.shadowRoot.appendChild(this._table);
+    }
+
+    protected _onRowClicked(event: CustomEvent<RowClickedEvent>) {
+        // ...
+    }
+
+    protected _onSelectionChanged(event: CustomEvent<SelectionChangedEvent>) {
+        // ...
+    }
+
+    private _onTableRenderError(event: CustomEvent<TableRenderErrorEvent>) {
+        const messageBox = document.querySelector('message-box') as MessageBox | null;
+        if (messageBox) {
+            messageBox.showMessage(
+                event.detail.message,
+                'error',
+                event.detail.error || ''
+            )
+        }
+    }
+
+    private _onSearchInputChange(event: Event) {
+        this._table.clearContent();
+        clearTimeout((this as any)._filterTimeout);
+        this._filterTimeout = setTimeout(() => {
+            this._table.filter = this._searchInput.value.trim();
+        }, 300);
+    }
+
+}
+
+/* -------------------------------------------------------------------------- */
+
+customElements.define('list-based-content', ListBasedContent);
+
+/* -------------------------------------------------------------------------- */
--- frontend/components/projects_content.ts
+++ frontend/components/projects_content.ts
@@ -1,5 +1,5 @@
 /* -------------------------------------------------------------------------- */
-
+import { ListBasedContent, ListBasedContentAttributes } from './list_based_content.js';
 import { MessageBox } from './base/message_box.js';
 import { StyledButton } from './base/styled_button.js';
 import { StyledInput } from './base/styled_input.js';
@@ -7,31 +7,18 @@ import { FilteredTable, RowClickedEvent, SelectionChangedEvent, TableRenderError
 
 /* -------------------------------------------------------------------------- */
 
-interface ProjectsContentAttributes {
-    contentUrl?: string;
+interface ProjectsContentAttributes extends ListBasedContentAttributes {
     projectGuiUrl?: string;
     scanUrl?: string;
-    sortImg?: string;
 }
 
 /* -------------------------------------------------------------------------- */
 
-export class ProjectsContent extends HTMLElement implements ProjectsContentAttributes {
+export class ProjectsContent extends ListBasedContent implements ProjectsContentAttributes {
     private _projectGuiUrl: string = '';
     private _scanUrl: string = '';
 
-    private _searchInput: StyledInput = new StyledInput();
     private _scanButton: StyledButton = new StyledButton();
-    private _table: FilteredTable = new FilteredTable();
-    private _filterTimeout: any = null;
-
-    get contentUrl() {
-        return this._table.contentUrl || '';
-    }
-
-    set contentUrl(value: string) {
-        this.setAttribute('content-url', value);
-    }
 
     get projectGuiUrl() {
         return this._projectGuiUrl;
@@ -49,122 +36,55 @@ export class ProjectsContent extends HTMLElement implements ProjectsContentAttri
         this.setAttribute('scan-url', value);
     }
 
-    get sortImg() {
-        return this._table.sortImg || '';
-    }
-
-    set sortImg(value: string) {
-        this.setAttribute('sort-img', value);
-    }
-
     constructor() {
         super();
-        this.attachShadow({ mode: 'open' });
 
-        this._populateShadowRoot();
+        this._populateChildShadowRoot();
     }
 
     static get observedAttributes() {
-        return ['content-url', 'project-gui-url', 'scan-url', 'sort-img'];
+        return [...super.observedAttributes, 'project-gui-url', 'scan-url'];
     }
 
     attributeChangedCallback(propertyName: string, oldValue: string | null, newValue: string | null) {
         if (oldValue === newValue) return;
 
-        if (propertyName === 'content-url') {
-            this._table.contentUrl = newValue || '';
-        } else if (propertyName === 'project-gui-url') {
+        super.attributeChangedCallback(propertyName, oldValue, newValue);
+
+        if (propertyName === 'project-gui-url') {
             this._projectGuiUrl = newValue || '';
         } else if (propertyName === 'scan-url') {
             this._scanUrl = newValue || '';
-        } else if (propertyName === 'sort-img') {
-            this._table.sortImg = newValue || '';
         }
     }
 
     connectedCallback() {
-        this.shadowRoot?.addEventListener('row-clicked', (event: Event) => this._onRowClicked(event as CustomEvent<RowClickedEvent>));
-        this.shadowRoot?.addEventListener('selection-changed', (event: Event) => this._onSelectionChanged(event as CustomEvent<SelectionChangedEvent>));
-        this.shadowRoot?.addEventListener('table-render-error', (event: Event) => this._onTableRenderError(event as CustomEvent<TableRenderErrorEvent>));
+        super.connectedCallback();
         this._scanButton.addEventListener('click', (event: Event) => this._onScanButtonClick(event));
-        this._searchInput.addEventListener('input', (event: Event) => this._onSearchInputChange(event));
     }
 
     disconnectedCallback() {
-        this.shadowRoot?.removeEventListener('row-clicked', (event: Event) => this._onRowClicked(event as CustomEvent<RowClickedEvent>));
-        this.shadowRoot?.removeEventListener('selection-changed', (event: Event) => this._onSelectionChanged(event as CustomEvent<SelectionChangedEvent>));
-        this.shadowRoot?.removeEventListener('table-render-error', (event: Event) => this._onTableRenderError(event as CustomEvent<TableRenderErrorEvent>));
+        super.disconnectedCallback();
+
         this._scanButton.removeEventListener('click', (event: Event) => this._onScanButtonClick(event));
-        this._searchInput.removeEventListener('input', (event: Event) => this._onSearchInputChange(event));
     }
 
-    private _populateShadowRoot() {
+    protected _getTableConfig() {
+        return '{"idColumn": "id", "headers": {"id": "ID", "name": "Name", "done": "Done", "open": "Open", "review": "Review", "created": "Created at", "updated": "Updated at"}, "filterElement": "filter"}';
+    }
+
+    private _populateChildShadowRoot() {
         if (!this.shadowRoot) {
             throw new Error("Shadow Root is not initialized.");
         }
 
-        const style = document.createElement('style');
-        style.textContent = `
-            :host {
-                display: grid;
-                grid-template-areas:
-                    "toolbar"
-                    "table";
-                grid-template-rows: auto 1fr;
-            }
-
-            .toolbar {
-                align-items: center;    
-                background-color: var(--background-color);
-                border-bottom: 1px solid var(--border-color);
-                display: grid;
-                grid-area: toolbar;
-                grid-template-areas: "scan search";
-                grid-template-columns: 1fr auto;
-                padding: 0.5rem 1rem;
-            }
-        
-            .toolbar > .buttons {
-                align-items: center;    
-                display: grid;
-                gap: .5rem;
-                grid-auto-columns: min-content;
-                grid-auto-flow: column;
-            }
-        `;
-
-        this.shadowRoot.appendChild(style);
-
-        const toolbar = document.createElement('div');
-        toolbar.classList.add('toolbar');
-        toolbar.style.gridArea = 'toolbar';
-
         this._scanButton.ariaLabel = "Schedule Scan";
         this._scanButton.textContent = "Schedule Scan";
         this._scanButton.disabled = true;
-        toolbar.appendChild(this._scanButton);
-
-        this._searchInput.type = "search";
-        this._searchInput.placeholder = "Filter Projects...";
-        this._searchInput.autocomplete = "off";
-        this._searchInput.autocorrect = "off";
-        this._searchInput.autocapitalize = "off";
-        this._searchInput.spellcheck = false;
-        this._searchInput.ariaLabel = "Filter Projects";
-        this._searchInput.style.gridArea = 'search';
-        this._searchInput.addEventListener('input', (event: Event) => this._onSearchInputChange(event));
-        toolbar.appendChild(this._searchInput);
-
-        this._table.style.gridArea = 'table';
-        this._table.ariaLabel = "Projects Table";
-        this._table.config = '{"idColumn": "id", "headers": {"id": "ID", "name": "Name", "done": "Done", "open": "Open", "review": "Review", "created": "Created at", "updated": "Updated at"}, "filterElement": "filter"}';
-        this._table.emptyMessage = 'No projects found';
-
-        this.shadowRoot.appendChild(toolbar)
-        this.shadowRoot.appendChild(this._table);
+        this._buttonsDiv.appendChild(this._scanButton);
     }
 
-    private _onRowClicked(event: CustomEvent<RowClickedEvent>) {
+    protected _onRowClicked(event: CustomEvent<RowClickedEvent>) {
         const detail = event.detail;
         const url = "";
         if (!detail.rowId) {
@@ -173,20 +93,9 @@ export class ProjectsContent extends HTMLElement implements ProjectsContentAttri
         window.location.href = this._projectGuiUrl.replace("[ID]", String(detail.rowId));
     }
 
-    private _onSelectionChanged(event: CustomEvent<SelectionChangedEvent>) {
+    protected _onSelectionChanged(event: CustomEvent<SelectionChangedEvent>) {
         const detail = event.detail;
         this._scanButton.disabled = detail.none;
-    }
-
-    private _onTableRenderError(event: CustomEvent<TableRenderErrorEvent>) {
-        const messageBox = document.querySelector('message-box') as MessageBox | null;
-        if (messageBox) {
-            messageBox.showMessage(
-                event.detail.message,
-                'error',
-                event.detail.error || ''
-            )
-        }
     }
 
     private _onScanButtonClick(event: Event) {
@@ -209,15 +118,6 @@ export class ProjectsContent extends HTMLElement implements ProjectsContentAttri
             });
         });
     }
-
-    private _onSearchInputChange(event: Event) {
-        this._table.clearContent();
-        clearTimeout((this as any)._filterTimeout);
-        this._filterTimeout = setTimeout(() => {
-            this._table.filter = this._searchInput.value.trim();
-        }, 300);
-    }
-
 }
 
 /* -------------------------------------------------------------------------- */
